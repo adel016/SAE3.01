a100b7d318a93a122b5249363adc6b8dc787225a
BoroHugreal <139383417+BoroHugreal@users.noreply.github.com>
2024-11-28 22:24:02 +0100

    Remodif

--- src/View/view.php
+++ src/View/view.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <title><?= htmlspecialchars($pagetitle); ?></title>
-        <link rel="stylesheet" href="/TD5/assets/css/style.css">
+        <link rel="stylesheet" href="/Assets/css/style.css">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Host+Grotesk:ital,wght@0,300..800;1,300..800&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Sour+Gummy:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
