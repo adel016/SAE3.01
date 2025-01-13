64294ed016a2f799b1efecc0f10c030badb6f8cd
BoroHugreal <139383417+BoroHugreal@users.noreply.github.com>
2025-01-13 14:23:57 +0100

    hehe

--- Web/frontController.php
+++ Web/frontController.php
@@ -1,5 +1,10 @@
 <?php
 
+// Demarrage de la session en premier
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
 require_once __DIR__ . '/../src/Lib/Psr4AutoloaderClass.php';
 
 use App\Meteo\Lib\Psr4AutoloaderClass;
